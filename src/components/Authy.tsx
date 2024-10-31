d6ebcbc5e4e339979e3d96f123deb81c7b6223be
Wazeer <58399263+wazeerc@users.noreply.github.com>
2024-10-31 11:47:39 +0400

    Refactor CSS styles: remove unused styles and update Authy component layout

--- src/components/Authy.tsx
+++ src/components/Authy.tsx
@@ -2,7 +2,7 @@ import { LoginForm } from "./Form";
 
 export const Authy = () => {
   return (
-    <div>
+    <div className="rounded-md border-2 border-solid border-purple-200 px-24 py-20 shadow-sm">
       <h1 className="mb-14 text-5xl text-purple-500">Welcome to Authy</h1>
       <LoginForm />
     </div>
