3216155ccdaa8a125459ad70ab6ce5d38820e07e
Wazeer <58399263+wazeerc@users.noreply.github.com>
2024-10-31 11:16:10 +0400

    Add LoginForm component and UI elements; update Authy component layout

--- src/components/Authy.tsx
+++ src/components/Authy.tsx
@@ -1,7 +1,10 @@
+import { LoginForm } from "./Form";
+
 export const Authy = () => {
   return (
     <div>
-      <h1 className="text-5xl text-purple-500">Welcome to Authy</h1>
+      <h1 className="mb-14 text-5xl text-purple-500">Welcome to Authy</h1>
+      <LoginForm />
     </div>
   );
 };
