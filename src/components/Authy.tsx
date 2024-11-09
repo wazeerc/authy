2b5a6b0d741f189ee1df869abd212eafc54f95af
Wazeer <58399263+wazeerc@users.noreply.github.com>
2024-11-09 10:13:06 +0400

    feat: add registration link to Authy component

--- src/components/Authy.tsx
+++ src/components/Authy.tsx
@@ -6,6 +6,14 @@ export const Authy = () => {
     <div className={cn("rounded-md border-2 border-solid border-purple-200 px-24 py-20 shadow-sm")}>
       <h1 className={cn("mb-14 text-5xl text-purple-500")}>Welcome to Authy</h1>
       <LoginForm />
+      <span className={cn("mt-4 block")}>
+        <a
+          href="/register"
+          className={cn("text-neutral-700 underline hover:cursor-pointer hover:text-neutral-500")}
+        >
+          Not a user? Register now.
+        </a>
+      </span>
     </div>
   );
 };
