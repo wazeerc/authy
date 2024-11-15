e7e43dc0630bd7f160738ac8e356c9ca4ca1cd02
Wazeer <58399263+wazeerc@users.noreply.github.com>
2024-11-15 17:27:51 +0400

    feat: Increase border thickness in Login and Register components for improved visibility

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -24,7 +24,7 @@ export const Login = () => {
         <Loader />
       ) : (
         <div
-          className={cn("rounded-md border-2 border-solid border-purple-200 px-24 py-20 shadow-sm")}
+          className={cn("rounded-md border-4 border-solid border-purple-200 px-24 py-20 shadow-sm")}
         >
           <h1 className={cn("mb-14 text-5xl text-purple-500")}>Welcome to Authy</h1>
           <AuthForm
