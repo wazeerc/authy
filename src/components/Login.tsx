a288327310b5f78b0b5272bf5f1738f8616d99f8
Wazeer <58399263+wazeerc@users.noreply.github.com>
2024-11-23 21:50:07 +0400

    feat: Add authentication state management to Login component and Zustand store

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -14,6 +14,7 @@ export const Login = () => {
 
   const handleSubmit = async (values: Credentials) => {
     try {
+      useStore.setState({ isAuthenticated: true });
       useStore.setState({ activeUserName: values.username });
       await mutateAsync(values);
     } catch (error) {
--- src/store/ZustandStore.tsx
+++ src/store/ZustandStore.tsx
@@ -3,11 +3,17 @@ import { create } from "zustand";
 interface StoreState {
   activeUserName: string;
   setActiveUserName: (name: string) => void;
+
+  isAuthenticated: boolean;
+  setIsAuthenticated: (value: boolean) => void;
 }
 
 const useStore = create<StoreState>(set => ({
   activeUserName: "",
   setActiveUserName: (name: string) => set({ activeUserName: name }),
+
+  isAuthenticated: false,
+  setIsAuthenticated: (value: boolean) => set({ isAuthenticated: value }),
 }));
 
 export default useStore;
