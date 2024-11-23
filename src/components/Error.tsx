9f4cc56d6513f7cde8a18a7b58967cbb376bb164
Wazeer <58399263+wazeerc@users.noreply.github.com>
2024-11-23 21:49:53 +0400

    feat: Enhance Error component with toast notification and navigation to login

--- src/components/Error.tsx
+++ src/components/Error.tsx
@@ -1,4 +1,27 @@
-export const ErrorPage = () => {
+import { useEffect } from "react";
+import { toast } from "@/hooks/use-toast";
+import { ToastAction } from "@radix-ui/react-toast";
+import { useNavigate } from "react-router-dom";
+
+type ErrorPageProps = {
+  toastErrorMsg: string;
+};
+export const Error = (props: ErrorPageProps) => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    toast({
+      description: `${props.toastErrorMsg}`,
+      variant: "destructive",
+      action: (
+        <ToastAction altText="Login" onClick={() => navigate("/login")}>
+          Login
+        </ToastAction>
+      ),
+    });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   return (
     <h1 className="text-6xl font-medium text-gray-500">404 Page Not Found</h1>
   );
